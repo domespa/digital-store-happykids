a699db61ba936a8dc36ce868327734fbd739e164
domespa <dumiii1988@gmail.com>
2026-01-19 15:27:11 +0100

    423dg

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -14,8 +14,9 @@ interface EmailOptions {
 // CONF NAMECHEAPCHOP
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "mail.privateemail.com",
-  port: parseInt(process.env.EMAIL_PORT || "465"),
-  secure: true,
+  port: parseInt(process.env.EMAIL_PORT || "587"),
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
