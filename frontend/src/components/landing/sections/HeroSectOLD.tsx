7733378bcc69f09ea75a471c7cd35c63a95aed19
domespa <dumiii1988@gmail.com>
2026-01-23 18:23:17 +0100

    423f

--- frontend/src/components/landing/sections/HeroSectOLD.tsx
+++ frontend/src/components/landing/sections/HeroSectOLD.tsx
@@ -5,8 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowDown,
   faArrowRight,
-  faStar,
-  faStarHalfStroke,
   faDownload,
 } from "@fortawesome/free-solid-svg-icons";
 
