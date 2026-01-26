68421e6ac1b750223814556bcf38a279bd75bee2
domespa <dumiii1988@gmail.com>
2026-01-26 09:31:39 +0100

    ewx

--- frontend/src/components/landing/sections/HeroSect.tsx
+++ frontend/src/components/landing/sections/HeroSect.tsx
@@ -1,6 +1,4 @@
 import { useLandingContext } from "../../../context/LandingContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function HeroSectV4() {
   const { config, isLoading } = useLandingContext();
