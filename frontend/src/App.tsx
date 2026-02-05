99367a83fca195909d13fcccb6b3a5c29459c430
domespa <dumiii1988@gmail.com>
2026-02-05 09:43:45 +0100

    rm consolelog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
+console.log = () => {};
+console.debug = () => {};
+console.info = () => {};
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
